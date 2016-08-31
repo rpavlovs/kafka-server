067af7c1fa5f0ffad785981ec8d20b627caca4f1
Roman Pavlovskyi <rpavlovskyi@gmail.com>
2016-08-31 00:19:28 -0400

    Add custom 404 handler

--- server/app.js
+++ server/app.js
@@ -1,14 +1,16 @@
-var co         = require('co')
-var koa        = require('koa')
-var route      = require('koa-route')
+let co         = require('co')
+let koa        = require('koa')
+let route      = require('koa-route')
+let onerror    = require('koa-onerror')
+let models     = require('./models')
+let userRoutes = require('./routes/user')
 
-var models     = require('./models')
-var userRoutes = require('./routes/user')
-
-var app = module.exports = koa()
+let app = module.exports = koa()
 
 
-co(setupAndStart).catch(onerror)
+let port = null
+
+co(setupAndStart).catch(onError)
 
 
 /**
@@ -16,22 +18,80 @@ co(setupAndStart).catch(onerror)
  */
 function *setupAndStart() {
   
+  // Try to get port from env
+  port = normalizePort(process.env.PORT || '3000')
+
   // Connect to DB and sync models
   yield models.sequelize.sync()
 
+  // Setup error handling
+  onerror(app)
+
   // Setup routes
   app.use(route.post('/classes/user/', userRoutes.add))
+  app.use(onNotFound)
   
-  app.listen(3000)
+  app.listen(port)
   console.log('Connected to database and listening on port 3000')
 
 }
 
 
 /**
- * Server error handler
+ * Normalize a port into a number, string, or false.
+ */
+function normalizePort(val) {
+  let port = parseInt(val, 10)
+
+  if (isNaN(port)) {
+    // named pipe
+    return val
+  }
+
+  if (port >= 0) {
+    // port number
+    return port
+  }
+
+  return false
+}
+
+
+/**
+ * Custom handler for 404 error
+ */
+function *onNotFound() {
+  this.body = '404 Not Found'
+}
+
+
+/**
+ * Event listener for HTTP server "error" event.
  * @param {error} err object containing error info
  */
-function onerror(err) {
+function onError(err) {
+
+  if (err.syscall !== 'listen') {
+    throw err
+  }
+
+  let bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port
+
+  // handle specific listen errors with friendly messages
+  switch (err.code) {
+  case 'EACCES':
+    console.error(bind + ' requires elevated privileges')
+    process.exit(1)
+    break
+  case 'EADDRINUSE':
+    console.error(bind + ' is already in use')
+    process.exit(1)
+    break
+  default:
+    throw err
+  }
   console.error(err.stack)
+
 }
