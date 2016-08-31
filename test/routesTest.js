5608f670258f0098c60a43db492de2d06dcbf172
Roman Pavlovskyi <rpavlovskyi@gmail.com>
2016-08-31 02:03:33 -0400

    Add integrations tests for user updates

--- test/routesTest.js
+++ test/routesTest.js
@@ -3,98 +3,167 @@ let app       = require('../server/app')
 let models    = require('../server/models')
 
 
-describe('User routes', () => {
+describe('/classes/user', () => {
 
   let server = supertest(app.listen())
 
 
-  beforeEach( function* () {
-    // drops and re-creates all tables
-    yield models.sequelize.sync({ force : true })
+  describe('#create', () => {
+
+    beforeEach( function* () {
+      // drop and re-creates all tables
+      yield models.sequelize.sync({ force : true })
+    })
+
+
+    it('should create a user', function* () {
+
+      let testUser = {
+        name    : 'Marcus',
+        email   : 'm@rc.us',
+        password: '123',
+      }
+      
+      let r = yield server.post('/classes/user').send(testUser).end()
+
+      r.status.should.equal(200)
+
+      r.body.should.have.property('password', testUser.password)
+      r.body.should.have.property('name', testUser.name)
+      r.body.should.have.property('email', testUser.email)
+      r.body.should.have.property('id')
+      r.body.should.have.property('updatedAt')
+      r.body.should.have.property('createdAt')
+
+      r.body.should.not.have.property('id', null)
+      r.body.should.not.have.property('updatedAt', null)
+      r.body.should.not.have.property('createdAt', null)
+    
+    })
+
+
+    it('should not be create a user without name', function* () {
+
+      let testUser = {
+        email   : 'm@rc.us',
+        password: '123',
+      }
+      
+      let r = yield server.post('/classes/user').send(testUser).end()
+
+      r.status.should.equal(400)
+
+    })
+
+
+    it('should not create a user without email', function* () {
+
+      let testUser = {
+        name    : 'Marcus',
+        password: '123',
+      }
+      
+      let r = yield server.post('/classes/user').send(testUser).end()
+
+      r.status.should.equal(400)
+
+    })
+
+
+    it('should not create a user without password', function* () {
+
+      let testUser = {
+        name    : 'Marcus',
+        email   : 'm@rc.us',
+      }
+      
+      let r = yield server.post('/classes/user').send(testUser).end()
+
+      r.status.should.equal(400)
+
+    })
+
+
+    it('should strip down extra data fields', function* () {
+
+      let testUser = {
+        name      : 'Marcus',
+        email     : 'm@rc.us',
+        password  : '123',
+        extraStaff: 'qwerty',
+      }
+      
+      let r = yield server.post('/classes/user').send(testUser).end()
+
+      r.body.should.not.have.property('extraStaff')
+
+    })
+
   })
 
 
-  it('should create a user', function* () {
 
-    let testUser = {
+  describe('#update', () => {
+
+    let user = {}
+    let testUserData = {
       name    : 'Marcus',
       email   : 'm@rc.us',
       password: '123',
     }
+
+    beforeEach( function* () {
+      
+      // drop and re-creates all tables
+      yield models.sequelize.sync({ force : true })
+      
+      // create test user
+      user = yield server.post('/classes/user').send(testUserData).end()
     
-    let r = yield server.post('/classes/user').send(testUser).end()
+    })
 
-    r.status.should.equal(200)
 
-    r.body.should.have.property('password', testUser.password)
-    r.body.should.have.property('name', testUser.name)
-    r.body.should.have.property('email', testUser.email)
-    r.body.should.have.property('id')
-    r.body.should.have.property('updatedAt')
-    r.body.should.have.property('createdAt')
+    it('should update user name only', function* () {
 
-    r.body.should.not.have.property('id', null)
-    r.body.should.not.have.property('updatedAt', null)
-    r.body.should.not.have.property('createdAt', null)
+      let update = {
+        name: 'MarcusUpdated'
+      }
+
+      let r = yield server.put('/classes/user' + user.id).send(update).end()
+      
+      r.body.name.should.equal(update.name)
+      r.body.email.should.equal(testUserData.email)
+      r.body.password.should.equal(testUserData.password)
+
+    })
+
+    it('should update user password only', function* () {
+
+      let update = {
+        password: '123'
+      }
+
+      let r = yield server.put('/classes/user' + user.id).send(update).end()
+      
+      r.body.password.should.equal(update.password)
+      r.body.name.should.equal(testUserData.name)
+      r.body.email.should.equal(testUserData.email)
+      
+    })
+
+
+    it('should not update user email', function* () {
+
+      let update = {
+        email: 'mUpdated@rc.us'
+      }
+
+      let r = yield server.put('/classes/user' + user.id).send(update).end()
+      
+      r.status.should.equal(400)
+      
+    })
   
-  })
-
-
-  it('should not be create a user without name', function* () {
-
-    let testUser = {
-      email   : 'm@rc.us',
-      password: '123',
-    }
-    
-    let r = yield server.post('/classes/user').send(testUser).end()
-
-    r.status.should.equal(400)
-
-  })
-
-
-  it('should not create a user without email', function* () {
-
-    let testUser = {
-      name    : 'Marcus',
-      password: '123',
-    }
-    
-    let r = yield server.post('/classes/user').send(testUser).end()
-
-    r.status.should.equal(400)
-
-  })
-
-
-  it('should not create a user without password', function* () {
-
-    let testUser = {
-      name    : 'Marcus',
-      email   : 'm@rc.us',
-    }
-    
-    let r = yield server.post('/classes/user').send(testUser).end()
-
-    r.status.should.equal(400)
-
-  })
-
-
-  it('should strip down extra fields', function* () {
-
-    let testUser = {
-      name      : 'Marcus',
-      email     : 'm@rc.us',
-      password  : '123',
-      extraStaff: 'qwerty',
-    }
-    
-    let r = yield server.post('/classes/user').send(testUser).end()
-
-    r.body.should.not.have.property('extraStaff')
-
   })
 
 })
