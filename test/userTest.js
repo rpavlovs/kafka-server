0df7a559a77d6e755c0276615aab2cec7e7b704c
Roman Pavlovskyi <rpavlovskyi@gmail.com>
2016-08-31 01:39:39 -0400

    Add user update unit tests

--- test/userTest.js
+++ test/userTest.js
@@ -9,34 +9,74 @@ describe('User', () => {
     password: 'pwd'
   }
 
-
   before(function* () {
     user = yield models.User.create(testUser)
   })
 
 
-  it('should have a name', () => {
-    user.should.have.property('name', testUser.name)
+  describe('create', () => {
+  
+    it('should store name', () => {
+      user.should.have.property('name', testUser.name)
+    })
+
+
+    it('should store password', () => {
+      user.should.have.property('password', testUser.password)
+    })
+
+
+    it('should store email', () => {
+      user.should.have.property('email', testUser.email)
+    })
+
+
+    it('should store creation date', () => {
+      user.should.not.have.property('createdAt', null)
+    })
+
+
+    it('should store last update date', () => {
+      user.should.not.have.property('updatedAt', null)
+    })
+
   })
 
 
-  it('should have a password', () => {
-    user.should.have.property('password', testUser.password)
-  })
+
+  describe('update', () => {
+
+    it('should change name', function *() {
+      yield user.updateAttributes({name: 'tstUpdate'})
+      user.name.should.equal('tstUpdate')
+    })
 
 
-  it('should have an email', () => {
-    user.should.have.property('email', testUser.email)
-  })
+    it('should change email', function *() {
+      yield user.updateAttributes({name: 'testUpdate@test.com'})
+      user.name.should.equal('testUpdate@test.com')
+    })
 
 
-  it('should have a not null creation date', () => {
-    user.should.not.have.property('createdAt', null)
-  })
+    it('should change password', function *() {
+      yield user.updateAttributes({name: 'pwdUpdate'})
+      user.name.should.equal('pwdUpdate')
+    })
 
 
-  it('should have a not null last update date', () => {
-    user.should.not.have.property('updatedAt', null)
+    it('should change update date', function *() {
+      let lastUpdatedAt = user.updateAt
+      yield user.updateAttributes({name: 'tstUpdate'})
+      user.updatedAt.should.not.equal(lastUpdatedAt)
+    })
+
+
+    it('should not change creation date', function *() {
+      let lastCreatedAt = user.createdAt
+      yield user.updateAttributes({name: 'tstUpdate'})
+      user.createdAt.should.equal(lastCreatedAt)
+    })
+
   })
 
 })
